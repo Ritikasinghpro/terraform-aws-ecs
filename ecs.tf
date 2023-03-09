3ee2b1cb776ee59a266f93be75a00cf9cd289a75
ritika <ritika@tothenew.com>
2023-03-09 12:17:02 +0530

    added task definition resource group

--- ecs.tf
+++ ecs.tf
@@ -7,3 +7,30 @@ resource "aws_ecs_cluster" "ecs" {
 
 }
 
+resource "aws_ecs_task_definition" "my_taskdefinition" {
+  family                   = "my_taskdefinition"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "bridge"
+  execution_role_arn       = "arn:aws:iam::111111111111:role/execution-role"
+  task_role_arn            = "arn:aws:iam::111111111111:role/task-role"
+  container_definitions = jsonencode(
+    [{
+      name             = "nginx"
+      image            = "nginx:latest"
+      essential        = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 0
+          protocol      = "tcp"
+        }
+      ]
+      cpu               = 256
+      memory            = 512
+      mountPoints = []
+      volumesFrom = []
+    }]
+  )
+}
+
+
