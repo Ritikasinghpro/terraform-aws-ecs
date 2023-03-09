e0dbd7abe855d695e85a818a0840d9a0f47ae5a1
ritika <ritika@tothenew.com>
2023-03-09 17:42:20 +0530

    Added iam policy

--- iam_role.tf
+++ iam_role.tf
@@ -66,6 +66,42 @@ EOF
 }
 
 
+resource "aws_iam_role_policy" "assume-role" {
+  name   = "assume-role-permission"
+  role   = aws_iam_role.task_definition_role.name
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "sts:GetSessionToken",
+                "sts:DecodeAuthorizationMessage",
+                "sts:GetAccessKeyInfo",
+                "sts:GetCallerIdentity",
+                "sts:GetServiceBearerToken"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "sts:*",
+            "Resource": [
+                "arn:aws:iam::1111112:role/cognito-test",
+                "arn:aws:iam::1111112:role/test-cognito-role",
+                "arn:aws:iam::1111112:role/ses-access",
+                "arn:aws:iam::1111112:role/test-dynamo-access"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+
 
 
 
