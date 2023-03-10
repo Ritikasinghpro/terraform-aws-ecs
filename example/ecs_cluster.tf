54789838a32e49adc836171f30ca3ed4008ae85d
ritika <ritika@tothenew.com>
2023-03-10 11:41:53 +0530

    Added

--- example/ecs_cluster.tf
+++ example/ecs_cluster.tf
@@ -1,5 +1,5 @@
 module "ecs_cluster" {
-  source               = "git@github.com:Ritikasinghpro/terraform-aws-ecs.git"
+  source               = "git@github.com:Ritikasinghpro/terraform-aws-ecs.git?ref=b1"
   name                 = var.name
   task_definition      = var.task_definition
   service              = var.service
@@ -9,26 +9,5 @@ module "ecs_cluster" {
 
 
 
-
-# module "task-definition" {
-#   source = "git@github.com:Ritikasinghpro/terraform-aws-ecs.git"
-#   name   = "nginx-td"
-#   family = "test-td"  #unique name for task defintion
-#   network_mode = "bridge"
-#   requires_compatibilities = "EC2"
-#   logConfiguration = {
-#     logDriver = "awslogs"
-#     options = {
-#       awslogs-group  = "awslogs-test"
-#       awslogs-region = "us-east-1"
-#     }
-#   }
-#   portMappings = [
-#     {
-#       containerPort = 8080
-#       protocol      = "TCP"
-#     }
-#   ]
-# }
 
 
